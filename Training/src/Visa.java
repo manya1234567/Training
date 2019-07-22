7be5f80897617f8ca71f11344201a8e7a2b6940a
Optimum <Optimum@TRAINING002.optimum.net>
2019-07-22 17:13:56 +0800

    first commit

---
+++ Training/src/Visa.java
@@ -0,0 +1,15 @@
+
+public class Visa {
+	void ageLimit() {
+		System.out.println("Age should not be less than 18");
+	}
+
+}
+
+class CitiBank extends Visa{
+	@Override
+	void ageLimit () {
+		
+		
+	}
+}
